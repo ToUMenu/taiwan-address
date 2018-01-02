6c1a3844888f69c6626c4722863d4e7d2c5fc8a3
DONGSHIH TENG <tou9702@gmail.com>
2018-01-03 01:27:48 +0900

    :cop:

--- spec/taiwan_address/reader_spec.rb
+++ spec/taiwan_address/reader_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe TaiwanAddress::Reader do
-  describe 'it should check alphabet?' do
-    it 'should return true on alphabet' do
+  describe 'Code 222	Shenkeng District	深坑區' do
+    it 'should return zone' do
       expect(described_class.new(222).zone).to eq "New Taipei City"
     end
   end
