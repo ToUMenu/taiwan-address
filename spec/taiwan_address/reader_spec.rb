6950586365a081f518ddbb19647ca6b0da486763
DONGSHIH TENG <tou9702@gmail.com>
2018-01-03 12:46:17 +0900

    Test case

--- spec/taiwan_address/reader_spec.rb
+++ spec/taiwan_address/reader_spec.rb
@@ -1,6 +1,30 @@
 require 'spec_helper'
 
 describe TaiwanAddress::Reader do
+  describe 'Code 106 Shenkeng District 深坑區' do
+    let(:shenkeng) do
+      described_class.new(106, :en)
+    end
+
+    it 'should return zone' do
+      expect(shenkeng.zone).to eq "Taipei City"
+    end
+
+    it 'should return address' do
+      expect(shenkeng.address).to eq "Taipei City Da An District"
+    end
+
+    it 'should return zone with Traditional Chinese' do
+      shenkeng.locale = :"zh-TW"
+      expect(shenkeng.zone).to eq "台北市"
+    end
+
+    it 'should return address with Traditional Chinese' do
+      shenkeng.locale = :"zh-TW"
+      expect(shenkeng.address).to eq "台北市 大安區"
+    end
+  end
+
   describe 'Code 222 Shenkeng District 深坑區' do
     let(:shenkeng) do
       described_class.new(222, :en)
