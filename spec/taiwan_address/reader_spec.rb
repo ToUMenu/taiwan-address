18a442b7dc6e42e0a555f7c5426d4f75c000d132
SHIHUA TENG <sika8204@gmail.com>
2018-01-03 13:11:57 +0900

    add test-code to 300

--- spec/taiwan_address/reader_spec.rb
+++ spec/taiwan_address/reader_spec.rb
@@ -2,170 +2,175 @@ require 'spec_helper'
 
 describe TaiwanAddress::Reader do
   describe 'Code 106' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(106, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "Taipei City"
+      expect(instance.zone).to eq "Taipei City"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq "Da An District"
+      expect(instance.district).to eq "Da An District"
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "Taipei City Da An District"
+      expect(instance.address).to eq "Taipei City Da An District"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "台北市"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "台北市"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "台北市 大安區"
+    it 'should return district with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.district).to eq "大安區"
+    end
+
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "台北市 大安區"
     end
   end
 
   describe 'Code 222' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(222, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "New Taipei City"
+      expect(instance.zone).to eq "New Taipei City"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq "Shenkeng District"
+      expect(instance.district).to eq "Shenkeng District"
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "New Taipei City Shenkeng District"
+      expect(instance.address).to eq "New Taipei City Shenkeng District"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "新北市"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "新北市"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "新北市 深坑區"
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "新北市 深坑區"
     end
   end
 
   describe 'Code 203' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(203, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "Keelung City"
+      expect(instance.zone).to eq "Keelung City"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq "Zhongshan District"
+      expect(instance.district).to eq "Zhongshan District"
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "Keelung City Zhongshan District"
+      expect(instance.address).to eq "Keelung City Zhongshan District"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "基隆市"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "基隆市"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "基隆市 中山區"
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "基隆市 中山區"
     end
   end
 
   describe 'Code 209' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(209, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "Lienchiang County"
+      expect(instance.zone).to eq "Lienchiang County"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq "Nangan Township"
+      expect(instance.district).to eq "Nangan Township"
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "Lienchiang County Nangan Township"
+      expect(instance.address).to eq "Lienchiang County Nangan Township"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "連江縣"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "連江縣"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "連江縣 南竿鄉"
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "連江縣 南竿鄉"
     end
   end
 
   describe 'Code 267' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(267, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "Yilan County"
+      expect(instance.zone).to eq "Yilan County"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq "Datong Township"
+      expect(instance.district).to eq "Datong Township"
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "Yilan County Datong Township"
+      expect(instance.address).to eq "Yilan County Datong Township"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "宜蘭縣"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "宜蘭縣"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "宜蘭縣 大同鄉"
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "宜蘭縣 大同鄉"
     end
   end
 
   describe 'Code 300' do
-    let(:shenkeng) do
+    let(:instance) do
       described_class.new(300, :en)
     end
 
     it 'should return zone' do
-      expect(shenkeng.zone).to eq "Hsinchu City"
+      expect(instance.zone).to eq "Hsinchu City"
     end
 
     it 'should return district' do
-      expect(shenkeng.district).to eq nil
+      expect(instance.district).to eq nil
     end
 
     it 'should return address' do
-      expect(shenkeng.address).to eq "Hsinchu City"
+      expect(instance.address).to eq "Hsinchu City"
     end
 
-    it 'should return zone with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.zone).to eq "新竹市"
+    it 'should return zone with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.zone).to eq "新竹市"
     end
 
-    it 'should return address with Traditional Chinese' do
-      shenkeng.locale = :"zh-TW"
-      expect(shenkeng.address).to eq "新竹市"
+    it 'should return address with zh-TW' do
+      instance.locale = :"zh-TW"
+      expect(instance.address).to eq "新竹市"
     end
   end
 end
